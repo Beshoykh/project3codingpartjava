6ed87c321ade4e01f7d5dcf5443131600fc8434c
Beshoykhalil <peshoyfouad@gmail.com>
2024-03-19 18:43:38 -0400

    Third Commit

--- src/main/java/org/example/codingtask/T7A.java
+++ src/main/java/org/example/codingtask/T7A.java
@@ -7,11 +7,11 @@ public class T7A {
         //7.	Write a Java Program to print the first 10 numbers of Fibonacci series.
         Scanner scan =new Scanner(System.in);
         System.out.println("Please enter till what numbers you need to get Fibonacci Series");
-        int ser=scan.nextInt();
+        int series=scan.nextInt();
         int firstNumber=0;
         int secondNumber=1;
-        System.out.println("Fibonacci Series till " +  ser +" numbers is ");
-        for (int i=1; i<ser; i++){
+        System.out.println("Fibonacci Series till " +  series +" numbers is ");
+        for (int i=1; i<series; i++){
             System.out.print(firstNumber + " ,");
             int nextNumber= firstNumber+secondNumber;
             firstNumber=secondNumber;
